566d1af4f61168eb4db20055ca090c986e79b4bb
Amandine Cousin <cousin.amandine.pro@gmail.com>
2023-10-12 15:06:10 +0200

    Premier commit

---
+++ Example-project/Example_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_projectApp.swift
+//  Example-project
+//
+//  Created by Amandine Cousin on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Example_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
